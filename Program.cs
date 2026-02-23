8eeabf773f0963fc2ef2fd10a217c3de28d09710
ambientcowboy <holysignals@protonmail.com>
2026-02-22 23:38:04 -0800

    Created cases for tasks.

--- Program.cs
+++ Program.cs
@@ -44,7 +44,35 @@ public class Program
             int choice = int.Parse(Console.ReadLine());
             switch (choice)
             {
-                
+                case 1:
+                    Console.WriteLine();
+                    task1.DisplayTask(1);
+                    task2.DisplayTask(2);
+                    task3.DisplayTask(3);
+                    break;
+                case 2:
+                    Console.Write("Select item to update (1-3): ");
+                    int itemChoice = int.Parse(Console.ReadLine());
+                    switch (itemChoice)
+                    {
+                        case 1:
+                            task1.ToggleCompleted();
+                            Console.WriteLine("Updated Item 1");
+                            break;
+                        case 2:
+                            task2.ToggleCompleted();
+                            Console.WriteLine("Updated Item 2");
+                            break;
+                        case 3:
+                            task3.ToggleCompleted();
+                            Console.WriteLine("Updated Item 3");
+                            break;
+                    }
+                    break;
+                case 3:
+                    running = false;
+                    Console.WriteLine("Program Ended");
+                    break;
             }
         }
     }
