47b71d01fa80bfcc72d00ba23fab311a94ad1304
ambientcowboy <holysignals@protonmail.com>
2026-02-22 23:30:16 -0800

    Initalized Objects for class StudyTask

--- Program.cs
+++ Program.cs
@@ -30,6 +30,22 @@ public class Program
 {
     public static void Main(string [] args)
     {
-        
+        StudyTask task1 = new StudyTask("Title", 5, true);
+        StudyTask task2 = new StudyTask("Title", 5, false);
+        StudyTask task3 = new StudyTask("Title", 5, false);
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine();
+            Console.WriteLine("1. Display All Study Tasks");
+            Console.WriteLine("2. Update Study Task");
+            Console.WriteLine("3. Exit");
+            Console.Write("Enter Choice: ");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                
+            }
+        }
     }
 }
